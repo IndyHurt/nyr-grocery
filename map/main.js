1ea1d0a4ff124b989313a17fcf5d6ed2012f5dc6
Indy Hurt <indy.hurt@gmail.com>
2016-01-14 16:41:32 -0800

    update trackOutbound

--- map/main.js
+++ map/main.js
@@ -148,7 +148,7 @@ map = (function () {
         anchor.target = '_blank';
         anchor.textContent = label;
         anchor.addEventListener('click', function (event) {
-            trackOutboundLink(url, 'editing_residential_buildings', type);
+            trackOutboundLink(url, 'editing_grocery', type);
         }, false);
         el.appendChild(anchor);
         return el;
